c075c81737f31f08568e34a26c6492e42aedd2e1
Firasse123 <firasguizani2012@gmail.com>
2026-04-24 22:10:00 +0100

    solver N+1 Problem

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,45 +1,55 @@
-export const Query={
-    getAllCvs:async(parent,args,{prisma},info)=>{
-        return await prisma.cv.findMany();
+export const Query = {
+    hello: () => "Hello World!",
 
+    getAllCvs: async (parent, args, { prisma }, info) => {
+        return prisma.cv.findMany({
+            include: {
+                user: true,
+                skills: {
+                    include: { skill: true }
+                }
+            }
+        });
     },
 
-    getCvById:async(parent,{id},{prisma},info)=>{
-        const cv= prisma.findUnique({
-            where:{id}
-        })
-        if(!cv){
-             throw new Error(`CV with id ${id} not found`);
-        }
+    getCvById: async (parent, { id }, { prisma }, info) => {
+        const cv = await prisma.cv.findUnique({
+            where: { id },
+            include: {
+                user: true,
+                skills: {
+                    include: { skill: true }
+                }
+            }
+        });
+        if (!cv) throw new Error(`CV with id ${id} not found`);
         return cv;
     },
 
-    getAllUsers:async(parent,args,{prisma},info)=>{
-        return await prisma.user.findMany();
+    getAllUsers: async (parent, args, { prisma }, info) => {
+        return prisma.user.findMany({
+            include: { cvs: true }
+        });
     },
 
-    getUserById:async(parent,{id},{prisma},info)=>{
-        const user=prisma.user.findUnique({
-            where:{id}
-        }
-    )
-        if(!user){
-            throw new Error(`User with id ${id} not found`);
-        }
+    getUserById: async (parent, { id }, { prisma }, info) => {
+        const user = await prisma.user.findUnique({
+            where: { id },
+            include: { cvs: true }
+        });
+        if (!user) throw new Error(`User with id ${id} not found`);
         return user;
     },
 
-    getAllSkills:async(parent,args,{prisma},info)=>{
-        return await prisma.skill.findMany();
+    getAllSkills: async (parent, args, { prisma }, info) => {
+        return prisma.skill.findMany();
     },
 
-    getSkillById:async(parent,{id},{prisma},info)=>{
-        const skill=prisma.skill.findUnique({
-            where:{id}
-        })
-        if(!skill){
-            throw new Error(`Skill with id ${id} not found`);
-        }
+    getSkillById: async (parent, { id }, { prisma }, info) => {
+        const skill = await prisma.skill.findUnique({
+            where: { id }
+        });
+        if (!skill) throw new Error(`Skill with id ${id} not found`);
         return skill;
     }
 }
